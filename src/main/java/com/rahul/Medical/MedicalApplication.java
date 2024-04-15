bd399720bd4eea069233ef3c75e5e9f045f39bcf
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-15 22:07:30 +0530

    Initial Commit

---
+++ src/main/java/com/rahul/Medical/MedicalApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.Medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahul/Medical/MedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.Medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
