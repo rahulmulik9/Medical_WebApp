6cdf9855cbd6bd63678b449ea3892b3d8807af6b
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-17 21:43:46 +0530

    Commented test code

--- src/test/java/com/rahul/Medical/MedicalApplicationTests.java
+++ src/test/java/com/rahul/Medical/MedicalApplicationTests.java
@@ -1,12 +1,12 @@
 package com.rahul.Medical;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MedicalApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
