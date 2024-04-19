fde8e74644bcc29f30976107d5bcbb4ac4786b6d
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-19 20:12:39 +0530

    AdminService

---
+++ src/main/java/com/rahul/Medical/service/AdminServiceImplementation.java
@@ -0,0 +1,50 @@
+package com.rahul.Medical.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rahul.Medical.entity.Admin;
+import com.rahul.Medical.repository.AdminRepository;
+
+@Service
+public class AdminServiceImplementation implements AdminService {
+
+	private AdminRepository adminRepository;
+
+	//inject employee dao
+	@Autowired   //Adding bean id @Qualifier
+	public AdminServiceImplementation( AdminRepository obj)
+	{
+		adminRepository=obj;
+	}
+	
+	@Override
+	public List<Admin> findAll() {
+		return adminRepository.findAll();
+	}
+
+	@Override
+	public void save(Admin admin)
+	{
+		
+		adminRepository.save(admin);
+	}
+
+	@Override
+	public Admin findByEmail(String user) {
+		// TODO Auto-generated method stub
+		
+		return adminRepository.findByEmail(user);
+		
+	}
+
+	@Override
+	public List<Admin> findByRole(String user) {
+		
+		return adminRepository.findByRole(user);
+	}
+
+	
+}
