a9bcda258857a342821b8465c31e951398d4283c
Rahul Mulik <rahulmulik9@gmail.com>
2024-04-24 18:28:44 +0530

    Login controller

---
+++ src/main/java/com/rahul/Medical/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.rahul.Medical.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+
+	@RequestMapping("/showMyLoginPage")
+	public String showHome()
+	{
+		
+		return "login";
+	}
+
+	
+/*
+	@RequestMapping("/authenticateTheUser")
+	public String shwHome()
+	{
+		
+		return "user/success";
+	}*/
+
+
+}
